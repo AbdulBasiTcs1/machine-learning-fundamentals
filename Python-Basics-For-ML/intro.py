3133c61a8396ba36739318c3386222e1e02331cf
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-27 20:56:15 +0500

    Python Basics for Machine Learning

---
+++ Python-Basics-For-ML/intro.py
@@ -0,0 +1,38 @@
+# Python Basics for Machine Learning 
+
+# Objective: 
+# This lab is an introductory session on Python. It is a powerful object-oriented programming language, comparable to 
+# Perl, Ruby, Scheme and Java. This lab will enable the students to learn the core building blocks used in AI based 
+# approaches. 
+
+# Activity Outcomes: 
+# The expected outcome of this lab activi 
+# Basic use of conditionals 
+# Basic use of loops 
+# Basic use of math libraries 
+# Developing a basic logic inference engine 
+
+# 1) Useful Concepts 
+# Programming is simply the act of entering instructions for the computer to perform. These instructions might 
+# crunch some numbers, modify text, look up information in files, or communicate with other computers over 
+# the Internet. All programs use basic instructions as building blocks. 
+# You can combine these building blocks to implement more intricate decisions, too. Programming is a 
+# creative task, somewhat like constructing a castle out of LEGO bricks. You start with a basic idea of what 
+# you want your castle to look like 
+# finished building your program, you can pretty up your code just like you would your castle. 
+# Python refers to the Python programming language (with syntax rules for writing what is considered valid 
+# Python code) and the Python interpreter software that reads source code (written in the Python language) 
+# and performs its instructions. The name Python comes from the surreal British comedy group Monty Python, 
+# not from the snake. Python programmers are affectionately called Pythonistas. 
+# the language rather than building professional software, then you should install from the Microsoft Store 
+# package. This offers the shortest and easiest path to getting started with minimal hassle. 
+# Windows environment, then the official Python.org 
+# limited by Microsoft Store policies, and you can control where the executable is installed and even add 
+# Python to PATH if necessary. 
+# In Python,a namespace is a collection of names and the details of the objects referenced by the names. We 
+# can consider a namespace as a python dictionary which maps object names to objects. The keys of the 
+# dictionary correspond to the names and the values correspond to the objects in python. In python, there are 
+# four types of namespaces, namely built-in namespaces,global namespaces, local namespaces and enclosing 
+# namespaces. 
+# Most recent releases of the python build can be downloaded from: 
+# https://www.python.org/downloads/windows/
