06aa90efb950aa89a08c874045030386d1638566
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-27 21:34:30 +0500

    Accept 5 integer values from user and display their sum. Draw flowchart before coding in python

---
+++ Python-Basics-For-ML/5numbers_sum.py
@@ -0,0 +1,9 @@
+# Accept 5 integer values from user and display their sum.
+summation=0
+i=0
+while i<=4 :
+    s=input("Enter number: ")
+    n=int(s)
+    summation=summation+n
+    i=i+1
+print("Sum is : ",summation)
