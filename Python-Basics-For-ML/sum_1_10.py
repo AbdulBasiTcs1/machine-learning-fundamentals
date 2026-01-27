ec464414054d4f6f4fbbae4ea57e4fbc02473486
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-27 21:32:18 +0500

    sum of all the values between 0-10 using while loop

---
+++ Python-Basics-For-ML/sum_1_10.py
@@ -0,0 +1,8 @@
+# Calculate the sum of all the values between 0-10 using while loop. 
+
+summation=0
+i=1
+while i<=10:
+    summation=summation+i
+    i=i+1
+print("The summation from 1 to 10 is: ",summation)
