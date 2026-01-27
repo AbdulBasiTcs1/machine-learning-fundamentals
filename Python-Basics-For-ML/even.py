548585ec7c8b8fe59c2daa1c6d172ab570774b26
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-27 21:14:40 +0500

    Let us take an integer from user as input and check whether the given value is even or not

---
+++ Python-Basics-For-ML/even.py
@@ -0,0 +1,4 @@
+# Let us take an integer from user as input and check whether the given value is even or not
+n = input("Enter a number")
+if int(n%2==0):
+    print("The given number is EVEN number")
