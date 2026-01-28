fae3a5f601f7557fe4458e9da5296c3134403ca2
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-28 09:18:50 +0500

    Intro to sets, tuples, dictionaries, and lists

---
+++ data&memory-usage-for-ml/intro.py
@@ -0,0 +1,16 @@
+# Understanding Data and Memory usage for Machine Learning
+
+# Objective: 
+# This lab will give you practical implementation of different types of sequences including lists, 
+# tuples, sets and dictionaries. We will use lists alongside loops in order to know about indexing 
+# individual items of these containers. This lab will also allow students to write their own functions. 
+
+# Activity Outcomes: 
+# ability to: 
+# Develop a basic understanding of data structures commonly used for AI 
+# Understanding Lists and Tupples 
+# Solving complex mathematical problems 
+# Using loops with lists 
+# Developing customized functions for logical inference 
+# Basic understanding of Object Oriented Concepts like Class definitions etc.
+
