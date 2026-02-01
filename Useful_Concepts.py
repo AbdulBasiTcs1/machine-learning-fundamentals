fb33792dba5e2afe5c6d7ff1605130e44453367e
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-01 19:21:55 +0500

    regression concepts of linear and non linear regression

---
+++ Useful_Concepts.py
@@ -0,0 +1,30 @@
+# # Useful Concepts
+ 
+# The two basic types of regression are simple linear regression and multiple linear regression, although there 
+# are non-linear regression methods for more complicated data and analysis. Simple linear regression uses 
+# one independent variable to explain or predict the outcome of the dependent variable Y, while multiple 
+# linear regression uses two or more independent variables to predict the outcome. 
+ 
+# Regression can help finance and investment professionals as well as professionals in other businesses. 
+# Regression can also help predict sales for a company based on weather, previous sales, GDP growth, or 
+# other types of conditions. The capital asset pricing model (CAPM) is an often-used regression model in 
+# finance for pricing assets and discovering costs of capital. 
+ 
+# The general form of each type of regression is: 
+ 
+# Simple linear regression: Y = a + bX + u 
+# Multiple linear regression: Y = a + b1X1 + b2X2 + b3X3 + ... + btXt + u 
+# Where: 
+# Y = the variable that you are trying to predict (dependent variable). 
+# X = the variable that you are using to predict Y (independent variable). 
+# a = the intercept. 
+# b = the slope. 
+# u = the regression residual. 
+# There are two basic types of regression: simple linear regression and multiple linear regression. 
+# Regression takes a group of random variables, thought to be predicting Y, and tries to find a mathematical 
+# relationship between them. This relationship is typically in the form of a straight line (linear regression) 
+# that best approximates all the individual data points. In multiple regression, the separate variables are 
+# differentiated by using subscripts. 
+# KEY TAKEAWAYS 
+# Regression helps investment and financial managers to value assets and understand the relationships 
+# between variables
