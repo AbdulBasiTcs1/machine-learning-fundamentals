0da2951211926d3f8bb96e9be2dddd4d5ad45d54
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-01 19:14:22 +0500

    objective of regression

---
+++ objective.py
@@ -0,0 +1,11 @@
+# # Objective: 
+# This lab will strengthen the concepts Machine Learning Algorithms like, Linear Regression, Multiple 
+# Linear Regression, Logistic Regression and Polynomial Regression. Regression is a statistical method used 
+# in finance, investing, and other disciplines that attempts to determine the strength and character of the 
+# relationship between one dependent variable (usually denoted by Y) and a series of other variables (known 
+# as independent variables).  
+ 
+# This lab will help the students to interpret the coefficients and statistical significance of a regression model. 
+# It will also help the students to apply regression to multiple datasets and interpret the results in the context of 
+# the problem domain. Lastly, the students will be able to evaluate the goodness of fit of a regression model and 
+# compare different models using metrics such as R-squared and adjusted R-squared. 
