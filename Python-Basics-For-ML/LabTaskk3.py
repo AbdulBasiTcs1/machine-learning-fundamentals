d58983d9d5f569ae037aa5319a4fe8e9673df471
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-09 00:10:54 +0500

    fibonaci series revision

--- Python-Basics-For-ML/LabTaskk3.py
+++ Python-Basics-For-ML/LabTaskk3.py
@@ -1,17 +1,10 @@
-#Fibonacci Series
-n=int(input("Enter the number : "))
-
+# Fibonacci series 
+number = int(input("Enter the number : "))
 a=0
 b=1
-
-if n<=0:
-    print("Please enter the positive")
-elif n==1:
-    print(a)
-else:
-    print(a,b,end=" ")
-    for i in range(2,n):
-        c=a+b
-        print(c,end=" ")
-        a=b
-        b=c
+for i in range(number):
+    print(a,end=" ")
+    c=a+b
+    a=b
+    b=c
+    
