3de8cde1dab937e6c48710dc85d70510124d9551
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-08 23:58:26 +0500

    calculate the factorial of a number

--- Python-Basics-For-ML/LabTask2.py
+++ Python-Basics-For-ML/LabTask2.py
@@ -1,9 +1,8 @@
-# Write a program that takes a number from user and calculate the factorial of that number. 
-num=int(input("Enter a number : "))
-factorial=1
-if num<0:
-    print("Factorial is not defined for the negative numbers")
-else:
-    for i in range(1,num+1):
-        factorial=factorial*i
-    print("Factorial is : ",factorial)
+# Write a program to calculate the factorial of a number 
+number = int(input("Enter the number to calculate the factorial : "))
+if number<0:
+    print("Factorial of a negative number is not possible : ")
+factoial=1
+for i in range(1,number+1):
+    factoial=factoial*i
+print("Factorial of number is : ",factoial)
